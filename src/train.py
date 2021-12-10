5459345335ff29ffb3844b6fe12bd29f7a60b12b
varad2302 <44045593+varad2302@users.noreply.github.com>
2021-12-11 04:09:18 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -3,25 +3,19 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 import torch.nn as nn
 import matplotlib
+import config
 import utils
-from model import FaceKeypointModel
-from landmarks import train_data, train_loader, valid_data, valid_loader
+
+from model import FaceKeypointResNet50
+from dataset import train_data, train_loader, valid_data, valid_loader
+
 from tqdm import tqdm
 matplotlib.style.use('ggplot')
 
-ROOT_PATH = 'Data/'
-OUTPUTS = 'Outputs/'
 
-BATCH_SIZE = 256
-LR = 0.0001
-EPOCHS = 300
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = FaceKeypointResNet50(pretrained=True, requires_grad=True).to(config.DEVICE)
 
-# model
-model = FaceKeypointModel().to(DEVICE)
-# optimizer
-optimizer = optim.Adam(model.parameters(), lr=LR)
-# we need a loss function which is good for regression like MSELoss
+optimizer = optim.Adam(model.parameters(), lr=config.LR)
 criterion = nn.MSELoss()
 
 
@@ -32,10 +26,10 @@ def fit(model, dataloader, data):
     train_running_loss = 0.0
     counter = 0
     # calculate the number of batches
-    num_batches = int(len(data) / dataloader.batch_size)
+    num_batches = int(len(data)/dataloader.batch_size)
     for i, data in tqdm(enumerate(dataloader), total=num_batches):
         counter += 1
-        image, keypoints = data['image'].to(DEVICE), data['keypoints'].to(DEVICE)
+        image, keypoints = data['image'].to(config.DEVICE), data['keypoints'].to(config.DEVICE)
         # flatten the keypoints
         keypoints = keypoints.view(keypoints.size(0), -1)
         optimizer.zero_grad()
@@ -44,41 +38,42 @@ def fit(model, dataloader, data):
         train_running_loss += loss.item()
         loss.backward()
         optimizer.step()
-
-    train_loss = train_running_loss / counter
+        
+    train_loss = train_running_loss/counter
+    
     return train_loss
 
 
-# validatioon function
+# validation function
 def validate(model, dataloader, data, epoch):
     print('Validating')
     model.eval()
     valid_running_loss = 0.0
     counter = 0
     # calculate the number of batches
-    num_batches = int(len(data) / dataloader.batch_size)
+    num_batches = int(len(data)/dataloader.batch_size)
     with torch.no_grad():
         for i, data in tqdm(enumerate(dataloader), total=num_batches):
             counter += 1
-            image, keypoints = data['image'].to(DEVICE), data['keypoints'].to(DEVICE)
+            image, keypoints = data['image'].to(config.DEVICE), data['keypoints'].to(config.DEVICE)
             # flatten the keypoints
             keypoints = keypoints.view(keypoints.size(0), -1)
             outputs = model(image)
             loss = criterion(outputs, keypoints)
             valid_running_loss += loss.item()
             # plot the predicted validation keypoints after every...
-            # ... 25 epochs and from the first batch
-            if (epoch + 1) % 25 == 0 and i == 0:
-                utils.plot_predictions(image, outputs, keypoints, epoch)
-
-    valid_loss = valid_running_loss / counter
+            # ... predefined number of epochs
+            if (epoch+1) % 1 == 0 and i == 0:
+                utils.valid_keypoints_plot(image, outputs, keypoints, epoch)
+        
+    valid_loss = valid_running_loss/counter
     return valid_loss
 
 
 train_loss = []
 val_loss = []
-for epoch in range(EPOCHS):
-    print(f"Epoch {epoch+1} of {EPOCHS}")
+for epoch in range(config.EPOCHS):
+    print(f"Epoch {epoch+1} of {config.EPOCHS}")
     train_epoch_loss = fit(model, train_loader, train_data)
     val_epoch_loss = validate(model, valid_loader, valid_data, epoch)
     train_loss.append(train_epoch_loss)
@@ -86,19 +81,19 @@ for epoch in range(EPOCHS):
     print(f"Train Loss: {train_epoch_loss:.4f}")
     print(f'Val Loss: {val_epoch_loss:.4f}')
 
-# loss plots
+# Plot training and validation loss
 plt.figure(figsize=(10, 7))
 plt.plot(train_loss, color='orange', label='train loss')
-plt.plot(val_loss, color='red', label='validation loss')
+plt.plot(val_loss, color='red', label='validataion loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
-plt.savefig(f"{OUTPUTS}loss.png")
+plt.savefig(f"{config.OUTPUT_PATH}/loss.png")
 plt.show()
 torch.save({
-            'epoch': EPOCHS,
+            'epoch': config.EPOCHS,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
             'loss': criterion,
-            }, f"{OUTPUTS}model.pth")
-print('DONE TRAINING')
+            }, f"{config.OUTPUT_PATH}/model.pth")
+print('TRAINING COMPLETE')
