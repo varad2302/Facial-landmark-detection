47bd5cec8b745a6c15ce6c3944b7f1207e5bfebc
varad2302 <44045593+varad2302@users.noreply.github.com>
2021-12-11 03:37:53 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import config
 
-ROOT_PATH = 'Data/'
-OUTPUTS = 'Outputs/'
 
-# Utility functions to make testing and visualization easier
-# Funtion to plot predicted keypoints and actual keypoints
-def plot_predictions(image, outputs, orig_keypoints, epoch):
+def valid_keypoints_plot(image, outputs, orig_keypoints, epoch):
+    """
+    This function plots the regressed (predicted) keypoints and the actual 
+    keypoints after each validation epoch for one image in the batch.
+    """
+    # detach the image, keypoints, and output tensors from GPU to CPU
     image = image.detach().cpu()
     outputs = outputs.detach().cpu().numpy()
     orig_keypoints = orig_keypoints.detach().cpu().numpy()
@@ -16,52 +18,35 @@ def plot_predictions(image, outputs, orig_keypoints, epoch):
     orig_keypoint = orig_keypoints[0]
     img = np.array(img, dtype='float32')
     img = np.transpose(img, (1, 2, 0))
-    img = img.reshape(96, 96)
-    plt.imshow(img, cmap='gray')
-
+    plt.imshow(img)
+    
     output_keypoint = output_keypoint.reshape(-1, 2)
     orig_keypoint = orig_keypoint.reshape(-1, 2)
     for p in range(output_keypoint.shape[0]):
         plt.plot(output_keypoint[p, 0], output_keypoint[p, 1], 'r.')
-        plt.text(output_keypoint[p, 0], output_keypoint[p, 1], f"{p}")
-        plt.plot(orig_keypoint[p, 0], orig_keypoint[p, 1], 'g.')
-        plt.text(orig_keypoint[p, 0], orig_keypoint[p, 1], f"{p}")
-    plt.savefig(f"{OUTPUTS}/val_epoch_{epoch}.png")
+        plt.plot(orig_keypoint[p, 0], orig_keypoint[p, 1], 'b.')
+    plt.savefig(f"{config.OUTPUT_PATH}/val_epoch_{epoch}.png")
     plt.close()
-
-
-# Function to plot test set images and keypoints
-def plot_test_set(images_list, outputs_list):
+    
+    
+def dataset_keypoints_plot(data):
+    """
+    This function shows the image faces and keypoint plots that the model
+    will actually see. This is a good way to validate that our dataset is in
+    fact corrent and the faces align wiht the keypoint features. The plot 
+    will be show just before training starts. Press `q` to quit the plot and
+    start training.
+    """
     plt.figure(figsize=(10, 10))
-    for i in range(len(images_list)):
-        outputs = outputs_list[i]
-        image = images_list[i]
-        outputs = outputs.cpu().detach().numpy()
-        outputs = outputs.reshape(-1, 2)
-        plt.subplot(3, 3, i+1)
-        plt.imshow(image, cmap='gray')
-        for p in range(outputs.shape[0]):
-                plt.plot(outputs[p, 0], outputs[p, 1], 'r.')
-                plt.text(outputs[p, 0], outputs[p, 1], f"{p}")
-        plt.axis('off')
-    plt.savefig(f"{OUTPUTS}/test_output.png")
-    plt.show()
-    plt.close()
-
-
-# Function to visualize the dataset
-def plot_dataset(data):
-    plt.figure(figsize=(20, 40))
-    for i in range(30):
+    for i in range(9):
         sample = data[i]
         img = sample['image']
         img = np.array(img, dtype='float32')
         img = np.transpose(img, (1, 2, 0))
-        img = img.reshape(96, 96)
-        plt.subplot(5, 6, i+1)
-        plt.imshow(img, cmap='gray')
+        plt.subplot(3, 3, i+1)
+        plt.imshow(img)
         keypoints = sample['keypoints']
         for j in range(len(keypoints)):
-            plt.plot(keypoints[j, 0], keypoints[j, 1], 'r.')
+            plt.plot(keypoints[j, 0], keypoints[j, 1], 'b.')
     plt.show()
     plt.close()
